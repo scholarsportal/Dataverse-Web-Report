3230afbc41c0611998f60be1a5daff8910275516
Kaitlin Newson <kaitlin.newson@utoronto.ca>
2019-04-10 16:04:08 -0400

    initial changes for angular 7 upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MultiselectComponent } from './multiselect/multiselect.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'Dataverse Report';
